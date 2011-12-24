e20d5752a10e13687a44dabc2a0fdf23b3b757e8
sablin.andrey <sablin.andrey@gmail.com>
2011-12-24 12:20:29 +0000

    Work on model test framework.

--- org.eclipse.wb.android.tests/src/org/eclipse/wb/tests/designer/android/tests/Activator.java
+++ org.eclipse.wb.android.tests/src/org/eclipse/wb/tests/designer/android/tests/Activator.java
@@ -8,7 +8,7 @@
  * Contributors:
  *    Google, Inc. - initial API and implementation
  *******************************************************************************/
-package org.eclipse.wb.android.tests.designer.tests;
+package org.eclipse.wb.tests.designer.android.tests;
 
 import java.io.InputStream;
 
